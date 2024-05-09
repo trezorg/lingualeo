ce762bc2629bf636d2e2035f48289b850aebc265
Igor Nemilentsev <trezorg@gmail.com>
2024-05-09 19:41:36 +0300

    Install release script

--- README.md
+++ README.md
@@ -4,6 +4,12 @@ lingualeo
 Lingualeo API console helper
 [![Build Status](https://travis-ci.com/trezorg/lingualeo.svg?branch=master)](https://travis-ci.com/trezorg/lingualeo)
 
+Download
+____________________________________
+
+    curl -sfL https://raw.githubusercontent.com/trezorg/lingualeo/main/install.sh | bash -s -- -d your_directory
+    curl -sfL https://raw.githubusercontent.com/trezorg/lingualeo/main/install.sh | bash -s
+
 Install
 ------------------------------------
 
---
+++ install.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+function usage() {
+	echo "Usage: bash install.sh [ -d directory ] [ -v version ]"
+	exit 2
+}
+
+INSTALL_DIR="${HOME}/bin"
+VERSION=""
+NAME=lingualeo
+OS=$(uname -o | tr '[:upper:]' '[:lower:]')
+ARCH=$(uname -m | tr '[:upper:]' '[:lower:]')
+OS="${OS##*/}"
+
+case "${ARCH}" in
+x86_64) ARCH="amd64" ;;
+arm) ARCH="arm64" ;;
+esac
+
+if which go &>/dev/null; then
+	gobin=$(go env GOBIN)
+	gopath="$(go env GOPATH)"
+	gopathbin="${gopath}/bin"
+	if [ -n "${gobin}" ] && [ -d "${gobin}" ]; then
+		INSTALL_DIR="${gobin}"
+	elif [ -n "${gopath}" ] && [ -d "${gopathbin}" ]; then
+		INSTALL_DIR="${gopathbin}"
+	fi
+fi
+
+while getopts hv:d: flag; do
+	case "${flag}" in
+	d) INSTALL_DIR=${OPTARG} ;;
+	v) VERSION=${OPTARG} ;;
+	h) usage ;;
+	*) usage ;;
+	esac
+done
+
+if [ ! -d "${INSTALL_DIR}" ]; then
+	echo "Directory ${INSTALL_DIR} does not exist"
+	exit 1
+fi
+if [ ! -w "${INSTALL_DIR}" ]; then
+	echo "Directory ${INSTALL_DIR} is not writable"
+	exit 1
+fi
+APP_PATH="${INSTALL_DIR}/${NAME}"
+
+echo "Installing into ${APP_PATH}..."
+
+if [ -z "${VERSION}" ]; then
+    VERSION=$(
+        curl -sSL --fail-with-body https://api.github.com/repos/trezorg/${NAME}/releases/latest | \
+        awk -F ':' '/tag_name/ { gsub("[\", ]", "", $2); print $2 }'
+    )
+fi
+DOWNLOAD_URL="https://github.com/trezorg/${NAME}/releases/download/${VERSION}/${NAME}-${OS}-${ARCH}"
+
+if ! curl -sSL --fail-with-body "${DOWNLOAD_URL}" -o "${APP_PATH}"; then
+	err=$?
+	echo "Failed to download ${DOWNLOAD_URL} into ${APP_PATH}"
+	exit ${err}
+fi
+
+chmod +x "${APP_PATH}"
+"${NAME}" --help
